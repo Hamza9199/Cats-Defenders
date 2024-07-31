c8935f4ad5aca1c59a0e28294aa7b2690c4f40a4
Hamza Gačić <hamza.gacic.22@size.ba>
2024-07-31 14:00:43 +0200

    Postavljanje projekta i igraca

---
+++ CatsDefenders/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Windows.Forms;
+
+
+namespace CatsDefenders
+{
+	public partial class Form1 : Form
+	{
+
+		private PictureBox igrac;
+		private int igracBrzina = 10;
+
+		public Form1()
+		{
+			PostaviIgru();
+		}
+
+		private void PostaviIgru()
+		{
+			this.Text = "Cats Defenders";
+			this.ClientSize = new Size(1360, 768);
+
+			igrac = new PictureBox
+			{
+				Slika = Image.FromFile("C:/Users/Korisnik/Desktop/private/nebitno/veoma bitno/igrac.png"),
+				Size = new Size(50, 50),
+				Location = new Point(this.ClientSize.Width / 2, this.ClientSize.Height - 60),
+				SizeMode = PictureBoxSizeMode.StretchImage
+			};
+
+			this.Controls.Add(igrac);
+
+			this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Left && igrac.Left > 0)
+			{
+				igrac.Left -= igracBrzina;
+			}
+
+			if (e.KeyCode == Keys.Right && igrac.Right < this.ClientSize.Width)
+			{
+				igrac.Left += igracBrzina;
+			}
+
+		}
+
+		
+	}
+}
---
+++ CatsDefenders/Form1.cs
@@ -0,0 +1,10 @@
+namespace CatsDefenders
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
